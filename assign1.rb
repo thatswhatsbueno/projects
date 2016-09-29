c198b37986b5a56c3406ece9636c45f5497735d7
thatswhatsbueno <bueno.andrew@gmail.com>
2016-09-29 10:45:10 +0000

    I committed a file.

---
+++ assign1.rb
@@ -0,0 +1,23 @@
+print "what is your name?"
+name = gets
+puts "Hello " + name + ". How are you?"
+
+#Create a ruby program that asks the user for a name and dob and stores it in a file.
+
+#print "What is your Name?"
+#years = gets
+
+
+#print "What is your Date of Birth?"
+
+
+#Create a ruby program that asks the user for a name, and an email, stores it in a file, and allows to bring up information if you give it an email.
+#  1. enter new name
+#  2. enter email
+#  2: jshernandez3@uh.edu
+
+#print "What is your name"
+
+#print "What is your email"
+
+
